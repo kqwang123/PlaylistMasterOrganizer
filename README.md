6a221944f64733788230631c3436da1d19a38239
kqwang123 <kqwang123@gmail.com>
2023-10-16 16:37:48 -0500

    Changed names to PlaylistMasterOrganizer

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# PutMeOn
+# PlaylistMasterOrganizer
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 16.2.4.
 
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'PutMeOn'`, () => {
+  it(`should have as title 'PlaylistMasterOrganizer'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('PutMeOn');
+    expect(app.title).toEqual('PlaylistMasterOrganizer');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('PutMeOn app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('PlaylistMasterOrganizer app is running!');
   });
 });
