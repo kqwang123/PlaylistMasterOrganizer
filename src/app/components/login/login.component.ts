fcf0b7be3ba966e5f29984671a7d6d0356cc6249
kqwang123 <kqwang123@gmail.com>
2023-10-16 17:22:22 -0500

    Emits login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -8,8 +8,7 @@ import { SpotifyService } from '../../services/spotify.service';
 })
 export class LoginComponent implements OnInit {
 
-  @Input() loggedIn: boolean = false;
-  @Output() loggedInChange: EventEmitter<boolean> = new EventEmitter<boolean>();
+  @Output() loggedIn: EventEmitter<boolean> = new EventEmitter<boolean>();
   
   constructor(private _spotifyService: SpotifyService) { }
 
@@ -17,7 +16,6 @@ export class LoginComponent implements OnInit {
 
   login(): void {
     this._spotifyService.login();
-    this.loggedIn = true;
-    this.loggedInChange.emit(this.loggedIn);
+    this.loggedIn.emit(true);
   }
 }
